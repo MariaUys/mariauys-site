922f4e9ecf5da71e0b2b5e7de9a9ed160df28f9b
MariaUys <cheekybuild@gmail.com>
2025-08-10 19:56:58 +0200

    feat: add service worker for offline caching

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'mariauys-site-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/afrigarde.html',
+  '/brand-identity.html',
+  '/graphic-design.html',
+  '/illustration.html',
+  '/packaging-design.html',
+  '/stylist.html',
+  '/textile-design.html',
+  '/art.html',
+  '/cv.html',
+  '/instagram-logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(networkResponse => {
+        if (
+          event.request.method === 'GET' &&
+          networkResponse &&
+          networkResponse.status === 200 &&
+          networkResponse.type === 'basic'
+        ) {
+          const respClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, respClone));
+        }
+        return networkResponse;
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
